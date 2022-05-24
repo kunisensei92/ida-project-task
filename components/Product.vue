eae7c9cc2fcc4dd841688fa3a681c6c3a5b01fbd
kunisensei92 <kuni@lean-system.co.jp>
2022-05-24 21:15:02 +0400

    make it better

---
+++ components/Product.vue
@@ -0,0 +1,60 @@
+<template>
+	<div class="list__item">
+		<div class="list__image">
+			<img :src="product.image" :alt="product.title">
+		</div>
+		<div class="list__header">
+			<h3 class="list__title">{{ product.title }}</h3>
+			<p class="list__description">{{ product.description }}</p>
+			<button class="delete-button" @click="deleteProduct(product.id)"></button>
+		</div>
+		<div class="list__footer">
+			<p class="list__price">{{ price }} руб.</p>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		product: Object
+	},
+	data() {
+		return {
+			price: ''
+		}
+	},
+	methods: {
+		deleteProduct(id) {
+			this.$emit('delete-product', id);
+		}
+	},
+	created() {
+		this.price = this.product.price.replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+	}
+}
+</script>
+
+<style lang="scss" scoped>
+
+
+.delete-button {
+	position: absolute;
+	top: -8px;
+	right: -8px;
+	background-image: url('~@/static/delete.svg');
+	background-position: 8px 8px;
+	background-repeat: no-repeat;
+	background-color: #FF8484;
+	border-width: 0;
+	border-radius: 10px;
+	width: 32px;
+	height: 32px;
+	cursor: pointer;
+}
+.delete-button:hover {
+	background-color: #e95c5c;
+}
+
+
+</style>
---
+++ components/ProductList.vue
@@ -0,0 +1,45 @@
+<template>
+	<TransitionGroup name="fade" tag="div">
+		<Product v-for="product in products" :key="product.id" @delete-product="$emit('delete-product', product.id)" :product="product" />
+	</TransitionGroup>
+</template>
+
+<script>
+import Product from './Product.vue';
+
+export default {
+    props: {
+        products: Array
+    },
+    components: { Product }
+}
+</script>
+
+<style lang="scss">
+
+.fade-enter {
+  opacity: 0;
+}
+
+.fade-enter-active {
+  transition: all .3s;
+}
+
+.fade-leave-to {
+  transition: all .3s;
+  opacity: 0;
+  transform: translateY(0);
+}
+
+.fade-leave-active {
+  transition: all .3s;
+  opacity: 0;
+  transform: translateY(0);
+  position: absolute !important;
+	width: 330px;
+}
+
+.fade-move {
+  transition: all .3s cubic-bezier(0.77, 0, 0.175, 1);
+}
+</style>
